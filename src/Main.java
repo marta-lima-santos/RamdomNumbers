d427b292cf959b291603a57c15e41f10e5660393
Marta Lima <121465689+marta-lima-santos@users.noreply.github.com>
2023-06-03 15:15:58 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Random; //Random is part of the java.util package
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Random randomD = new Random();
+        int x = randomD.nextInt(6)+1;
+        //You choice:
+        //double randomD = random.nextDouble();
+        //boolean randomD = random.nextBoolean();
+        System.out.println(x);
         }
     }
-}
